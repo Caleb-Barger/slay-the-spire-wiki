e8070d0f72b299b04bf3c5551f4cf376adfc4dc5
Caleb <bargercj20@hotmail.com>
2020-09-01 22:30:17 -0700

    documentation...

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,6 +2,6 @@ export const SET_CHARACTER = "SET_CHARACTER"
 
 // character should be Ironclad, Silent, Defect, or Watcher NOTHING ELSE!!
 
-export const setCharacter = character => {
-    return {type: SET_CHARACTER, payload: character}
+export const setCharacter = character => { 
+    return {type: SET_CHARACTER, payload: character} // TODO mk this better...
 }
